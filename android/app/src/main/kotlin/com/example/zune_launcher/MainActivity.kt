ec5bacb33ce164cf4b7adbc64ae5b10d6aa30017
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2023-07-12 06:38:45 +0500

    added android files

---
+++ android/app/src/main/kotlin/com/example/zune_launcher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zune_launcher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,31 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: _themeData(),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
+      ),
+    );
+  }
+
+  ThemeData _themeData() {
+    return ThemeData(
+      scaffoldBackgroundColor: Colors.black,
+      colorScheme: ColorScheme(
+        brightness: Brightness.dark,
+        primary: Colors.teal,
+        onPrimary: Colors.white,
+        secondary: Colors.teal.shade600,
+        onSecondary: Colors.white,
+        error: Colors.redAccent.shade700,
+        onError: Colors.white,
+        background: Colors.black,
+        onBackground: Colors.white,
+        surface: Colors.black,
+        onSurface: Colors.white,
       ),
     );
   }
